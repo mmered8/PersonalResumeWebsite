c13d93b3af8908cc1963e68a561d409bb6a2cf34
mmered8 <masonlockemeredith@98-fe-94-42-38-06.wlan.lsu.edu>
2018-02-19 18:50:02 -0600

    javascript exercise for code 3 assignment
    his simple javascript exercise

---
+++ js/code-assignment-03.js
@@ -0,0 +1,9 @@
+let projectId =1;
+let projectTitle = "Map1";
+let ProjectUrl = "https://example.com";
+let hasThumbnail = true;
+let projectKeywords = ['web gis', 'earthquakes'];
+console.log(projectId);
+console.log(projectTitle);
+console.log(ProjectUrl);
+console.log(hasThumbnail);
