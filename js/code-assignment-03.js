7905101fae7f8c97252a061872ed40a87626854b
mmered8 <masonlockemeredith@98-fe-94-42-38-06.wlan.lsu.edu>
2018-02-19 19:00:42 -0600

    update javascript
    stuff

--- js/code-assignment-03.js
+++ js/code-assignment-03.js
@@ -3,6 +3,7 @@ let projectTitle = "Map1";
 let ProjectUrl = "https://example.com";
 let hasThumbnail = true;
 let projectKeywords = ['web gis', 'earthquakes'];
+
 console.log(projectId);
 console.log(projectTitle);
 console.log(ProjectUrl);
