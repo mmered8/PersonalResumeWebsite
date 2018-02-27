f89b6143bb0a4375c7153279c6b30c37735fcb1c
mmered8 <masonlockemeredith@98-fe-94-42-38-06.wlan.lsu.edu>
2018-02-26 18:04:32 -0600

    forgot
    update to prev update

--- js/code-assignment-04.js
+++ js/code-assignment-04.js
@@ -1,7 +1,7 @@
 // This is an array
 let projectArray = [
   1,
-  'https://example.com',
+  'https://www.arcgis.com/apps/MapTour/index.html?appid=3a0f07d8a63d4512976a17637c7ee7a6',
   'Redlands',
   'Redlands MapTour.',
   true,
@@ -11,7 +11,7 @@ let projectArray = [
 // This is an object. Each value is paired with a property.
 let projectObject = {
   id: 1,
-  url: 'https://example.com',
+  url: 'https://llsu.maps.arcgis.com/apps/GeoForm/index.html?appid=40f9d5fb11764a9d888995bfdcad14c7',
   title: 'LSU Incidents',
   desc: 'Incidents both silly and harmful from LSU.',
   thumb: true,
