61ae3a6be01ccdd8761ee8dc149045367eaabdd1
mmered8 <masonlockemeredith@98-fe-94-42-38-06.wlan.lsu.edu>
2018-03-12 18:18:45 -0500

    nitpick

--- js/code-assignment-6a.js
+++ js/code-assignment-6a.js
@@ -1,6 +1,6 @@
-let projects = [];
+let projects = []
 projects[1] = {
-  id:1,
+  id: 1,
   url: 'http://bit,ly/2EF070t',
   title: 'RedLands Tour',
   desc: 'An ArcGis Online website about redlands CA.',
@@ -13,23 +13,23 @@ projects[2] = {
   title: 'Mall',
   desc: 'An ArcGis Online website about popular places in BR.',
   thumb: true,
-  keywords: ['Baton Rouge', Mall]
+  keywords: ['Baton Rouge', 'Mall']
 }
 for (let i = 0; i < projects.length; i++) {
   // log the current project's title
-  console.log('Index positon ' + i + ' title: ' + projects[i].title);
+  console.log('Index positon ' + i + ' title: ' + projects[i].title)
   // build and log an image file name based on the project ID
-  if (projects[i].thumb === true) {
-  console.log('images/' + projects[i].id + '.png');
+  if (projects[i].thumb === true){
+    console.log('images/' + projects[i].id + '.png')
   }
   else {
-    console.log('images/no-preview.png');
+    console.log('images/no-preview.png')
   }
 }
 
 function describeFruit (fruit) {
-  let description = 'The '+ fruit +' is a delicious snack.';
-  return description;
+  let description = 'The ' + fruit + ' is a delicious snack.'
+  return description
 }
-let fDesc = describeFruit('apple');
-console.log(fDesc);
+let fDesc = describeFruit('apple')
+console.log(fDesc)
