c7d9ad69386db82a7087c0d50200caacb91bf299
mmered8 <masonlockemeredith@98-fe-94-42-38-06.wlan.lsu.edu>
2018-03-12 18:04:35 -0500

    added and deleted stuff for coding assignment

---
+++ js/code-assignment-6a.js
@@ -0,0 +1,35 @@
+let projects = [];
+projects[1] = {
+  id:1,
+  url: 'http://bit,ly/2EF070t',
+  title: 'RedLands Tour',
+  desc: 'An ArcGis Online website about redlands CA.',
+  thumb: true,
+  keywords: ['REdlands', 'California', 'story map', 'arcgis online']
+}
+project[2] = {
+  id: 2,
+  url: 'https://www.arcgis.com/apps/MapTour/index.html?appid=34dab67ae97e4a12a4611f53ccc0566f',
+  title: 'Mall',
+  desc: 'An ArcGis Online website about popular places in BR.',
+  thumb: true,
+  keywords: ['Baton Rouge', Mall]
+}
+for (let i = 0; i < projects.length; i++) {
+  // log the current project's title
+  console.log('Index positon ' + i + ' title: ' + projects[i].title);
+  // build and log an image file name based on the project ID
+  if (projects[i].thumb === true) {
+  console.log('images/' + projects[i].id + '.png');
+  }
+  else {
+    console.log('images/no-preview.png');
+  }
+}
+
+function describeFruit (fruit) {
+  let description = 'The '+ fruit +' is a delicious snack.';
+  return description;
+}
+let fDesc = describeFruit('apple');
+console.log(fDesc);
