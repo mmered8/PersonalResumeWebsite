bf6b4f741523caca1d8e5c508e7c8be66103a389
mmered8 <masonlockemeredith@98-fe-94-42-38-06.wlan.lsu.edu>
2018-03-12 18:34:16 -0500

    spaces

--- js/code-assignment-6a.js
+++ js/code-assignment-6a.js
@@ -1,19 +1,19 @@
 let projects = []
 projects[1] = {
-  id: 1,
-  url: 'http://bit,ly/2EF070t',
-  title: 'RedLands Tour',
-  desc: 'An ArcGis Online website about redlands CA.',
-  thumb: true,
-  keywords: ['REdlands', 'California', 'story map', 'arcgis online']
+  id:1,
+  url:'http://bit,ly/2EF070t',
+  title:'RedLands Tour',
+  desc:'An ArcGis Online website about redlands CA.',
+  thumb:true,
+  keywords:['REdlands', 'California', 'story map', 'arcgis online']
 }
 projects[2] = {
-  id: 2,
-  url: 'https://www.arcgis.com/apps/MapTour/index.html?appid=34dab67ae97e4a12a4611f53ccc0566f',
-  title: 'Mall',
-  desc: 'An ArcGis Online website about popular places in BR.',
-  thumb: true,
-  keywords: ['Baton Rouge', 'Mall']
+  id:2,
+  url:'https://www.arcgis.com/apps/MapTour/index.html?appid=34dab67ae97e4a12a4611f53ccc0566f',
+  title:'Mall',
+  desc:'An ArcGis Online website about popular places in BR.',
+  thumb:true,
+  keywords:['Baton Rouge', 'Mall']
 }
 
 
