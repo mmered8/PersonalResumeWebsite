c16f537e36433842e814100c31a26b7eb691b408
mmered8 <masonlockemeredith@Masons-MacBook-Air.local>
2018-03-15 18:25:57 -0500

    updates!

--- js/code-assignment-6a.js
+++ js/code-assignment-6a.js
@@ -1,13 +1,13 @@
 let projects = []
-projects[1] = {
+projects[0] = {
   id:1,
   url:'http://bit,ly/2EF070t',
   title:'RedLands Tour',
   desc:'An ArcGis Online website about redlands CA.',
   thumb:true,
-  keywords:['REdlands', 'California', 'story map', 'arcgis online']
+  keywords:['Redlands', 'California', 'story map', 'arcgis online']
 }
-projects[2] = {
+projects[1] = {
   id:2,
   url:'https://www.arcgis.com/apps/MapTour/index.html?appid=34dab67ae97e4a12a4611f53ccc0566f',
   title:'Mall',
@@ -15,32 +15,40 @@ projects[2] = {
   thumb:true,
   keywords:['Baton Rouge', 'Mall']
 }
-function createTitle(i){
-  console.log('Index position ' + i + 'title: ' + projects[i].title + '')
-}
-function createImageSrc(i){
-  // build and log an image file name based on the project ID
-  if (projects[i].thumb === true){
-    console.log('images/' + projects[i].id + '.png')
-  }
-  else {
-    console.log('images/no-preview.png')
-  }
-}
+
 function loopAndLog(){
   for (let i = 0; i < projects.length; i++) {
     let title = createTitle(i)
     let image = createImageSrc(i)
-    console.log(title);
-    console.log(image);
+    console.log(title)
+    console.log(image)
   }
 }
+
+function createTitle(i){
+  var a ='Index position ' + i + ' title: ' + projects[i].title
+  return a
+}
+function createImageSrc(i){
+  // build and log an image file name based on the project ID
+  if (projects[i].thumb === true){
+    var a ='images/' + projects[i].id + '.png'
+    return a
+  }
+  else {
+    var b ='images/no-preview.png'
+    return b
+  }
+}
+
 loopAndLog()
 
 // Describes Fruit
+/*
 function describeFruit (fruit) {
   let description = 'The ' + fruit + ' is a delicious snack.'
   return description
 }
 let fDesc = describeFruit('apple')
 console.log(fDesc)
+*/
