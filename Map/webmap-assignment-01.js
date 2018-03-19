ba8577de21cfe692ed4a8b696a604c84ba73933c
mmered8 <masonlockemeredith@MacBook-Pro.local>
2018-03-19 16:58:03 -0500

    add

--- Map/webmap-assignment-01.js
+++ Map/webmap-assignment-01.js
@@ -1,1 +1,9 @@
 let mymap = L.map('mapid').setView([51.505, -0.09], 13)
+L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_nolabels/{z}/{x}/{y}.png').addTo(mymap)
+
+let marker = L.marker([51.5, -0.09]).addTo(mymap)
+let polygon = L.polygon([
+  [51.509, -0.08],
+  [51.503, -0.06],
+  [51.51, -0.047]
+]).addTo(mymap);
