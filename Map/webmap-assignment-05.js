2d0118353e649ac2aa9b0459292bc892d35c5a7b
mmered8 <mmered8@github.com>
2018-04-16 17:06:31 -0500

    map name update
    update

--- Map/webmap-assignment-05.js
+++ Map/webmap-assignment-05.js
@@ -1,23 +1,23 @@
 
 function map5() {
-let myMap = L.map('mapid3').setView([40, -100], 4)
-myMap.on('click', function (event) {
+let newMap5 = L.map('mapid3').setView([41, -101], 4)
+newMap5.on('click', function (event) {
   console.log('You clicked the map at ' + event.latlng)
 })
 
-let lightBasemap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(myMap)
-let darkBasemap = L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_nolabels/{z}/{x}/{y}.png')
-let soilBasemap = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png')
+let lightMap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(newMap5)
+let darkMap = L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_nolabels/{z}/{x}/{y}.png')
+let soilMap = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png')
 
 let basemaps = {
-   'Light basemap': lightBasemap,
-   'Dark basemap': darkBasemap,
-   'Soil basemap': soilBasemap
+   'Light basemap': lightMap,
+   'Dark basemap': darkMap,
+   'Soil basemap': soilMap
  }
 
- L.control.layers(basemaps).addTo(myMap)
+ L.control.layers(basemaps).addTo(newMap5)
 
- function myStyle (feature) {
+ function mapStyle (feature) {
    let population = feature.properties.POPULATION
    let color = 'blue'
    if (population < 6514000) {
@@ -28,17 +28,17 @@ let basemaps = {
      weight: 1,
      fillOpacity: 0.2
    }
-   return myStyle
+   return mapStyle
  }
- function myPopup (feature, layer) {
+ function mapPopup (feature, layer) {
    let name = feature.properties.STATE_NAME
    let population = feature.properties.POPULATION
    layer.bindPopup('Median population of ' + name + ': ' + population + '<br>National average per state: ~6,514,000 million')
  }
-  let myOptions = {
-    style: myStyle,
-    onEachFeature: myPopup
+  let mapOptions = {
+    style: mapStyle,
+    onEachFeature: mapPopup
   }
-  L.geoJSON(stateDemographics, myOptions).addTo(myMap)
+  L.geoJSON(stateDemographics, mapOptions).addTo(newMap5)
 }
 map5()
