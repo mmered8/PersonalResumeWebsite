31c473f2c06839348cd7bf903004209d0e9db522
mmered8 <mason.mere@gmail.com>
2024-03-09 16:32:21 -0600

    Form through AWS
    in theory first attempt

--- js/scripts.js
+++ js/scripts.js
@@ -3,5 +3,24 @@
 * Copyright 2013-2023 Start Bootstrap
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-personal/blob/master/LICENSE)
 */
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
+
+document.getElementById('submitButton').addEventListener('click', function() {
+    const data = {
+        name: document.getElementById('name').value,
+        email: document.getElementById('email').value,
+        number: document.getElementById('phone').value,
+        message: document.getElementById('message').value
+    };
+
+    fetch('https://hqqehdq4jb.execute-api.us-east-2.amazonaws.com/FormAPI', {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: {
+            'Content-Type': 'application/json'
+            // Include API key header if necessary
+        }
+    })
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.error('Error:', error));
+});
