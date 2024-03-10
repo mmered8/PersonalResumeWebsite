a089262c3c8dcb7c3c9240e8e61b1a8539764491
mmered8 <mason.mere@gmail.com>
2024-03-10 11:06:47 -0500

    removed
    the data token

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,29 @@
-/*!
-* Start Bootstrap - Personal v1.0.1 (https://startbootstrap.com/template-overviews/personal)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-personal/blob/master/LICENSE)
-*/
+// document.getElementById('submitButton').addEventListener('click', function() {
+//     const data = {
+//         name: document.getElementById('name').value,
+//         email: document.getElementById('email').value,
+//         number: document.getElementById('phone').value,
+//         message: document.getElementById('message').value
+//     };
 
-document.getElementById('submitButton').addEventListener('click', function() {
+//     fetch('https://hqqehdq4jb.execute-api.us-east-2.amazonaws.com/FormAPI', {
+//     method: 'POST',
+//     body: JSON.stringify(data),
+//     headers: {
+//         'Content-Type': 'application/json'
+//     }
+// })
+// .then(response => response.json())
+// .then(data => console.log(data))
+// .catch(error => console.error('Error:', error));
+
+// });
+
+
+
+function handleSubmit(event) {
+    event.preventDefault(); // Prevents default form submission behavior
+
     const data = {
         name: document.getElementById('name').value,
         email: document.getElementById('email').value,
@@ -17,10 +36,15 @@ document.getElementById('submitButton').addEventListener('click', function() {
         body: JSON.stringify(data),
         headers: {
             'Content-Type': 'application/json'
-            // Include API key header if necessary
         }
     })
     .then(response => response.json())
-    .then(data => console.log(data))
-    .catch(error => console.error('Error:', error));
-});
+    .then(data => {
+        console.log(data);
+        // You can add code here to update the UI on successful submission
+    })
+    .catch(error => {
+        console.error('Error:', error);
+        // Update the UI to show the error message
+    });
+}
